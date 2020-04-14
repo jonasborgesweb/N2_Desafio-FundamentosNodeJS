33edfd883522c18a9d78755c278e5dd1531ee594
jonasborgesweb <jonasborgesweb@gmail.com>
2020-04-14 15:56:27 -0300

    Finalizado o desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionsDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    // Somando os Valores das Transações do Tipo income
+    const income = this.transactions
+      .filter(({ type }) => type === 'income')
+      .reduce((cont, { value }) => cont + value, 0);
+
+    // Somando os Valores das Transações do Tipo outcome
+    const outcome = this.transactions
+      .filter(({ type }) => type === 'outcome')
+      .reduce((cont, { value }) => cont + value, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionsDTO): Transaction {
+    const transition = new Transaction({ title, value, type });
+
+    this.transactions.push(transition);
+
+    return transition;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    // Verficação se o valor da Transação é Maior que o Valor em Caixa
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance();
+
+      if (value > total) {
+        throw Error('You cannot have a negative balance');
+      }
+    }
+
+    // Criando a Transação
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
